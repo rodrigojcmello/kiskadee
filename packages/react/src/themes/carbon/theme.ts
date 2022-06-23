701e80cacfe7abb731c23346f657bbd12848847b
Rodrigo Mello <rodrigojcmello@gmail.com>
2022-06-23 12:20:56 -0300

    fix: themes schema

--- packages/react/src/themes/carbon/theme.ts
+++ packages/react/src/themes/carbon/theme.ts
@@ -7,204 +7,197 @@ export const carbonTheme: KiskadeeSchema = {
   kiskadeeVersion: '0.0.1',
   component: {
     button: {
-      container: {
-        option: {
-          widthMin: 100,
-          borderRadius: {
-            default: 0,
-            none: 0,
-          },
-          textAlign: {
-            default: 'left',
-            left: true,
-          },
-          icon: {
-            rightDetached: true,
-          },
+      option: {
+        widthMin: 100,
+        borderRadius: {
+          default: 'none',
+          none: 0,
         },
-        type: {
-          contained: {
-            variant: {
-              primary: {
-                rest: {
-                  md: {
-                    backgroundColor: '#0F62FE',
-                  },
-                },
-                hover: {
-                  md: {
-                    backgroundColor: '#0353E9 ',
-                  },
-                },
-                focus: {
-                  md: {
-                    outlineWidth: 1,
-                    outlineColor: '#ffffff',
-                    outlineStyle: 'solid',
-                    outlineOffset: -3,
-                  },
-                },
-                pressed: {
-                  md: {
-                    backgroundColor: '#002D9C',
-                  },
-                },
-                disabled: {
-                  md: {
-                    backgroundColor: '#E4E4E4',
-                  },
-                },
-              },
-              secondary: {
-                rest: {
-                  md: {
-                    backgroundColor: '#393939',
-                  },
-                },
-                hover: {
-                  md: {
-                    backgroundColor: '#474747',
-                  },
-                },
-                focus: {
-                  md: {
-                    outlineWidth: 1,
-                    outlineColor: '#ffffff',
-                    outlineStyle: 'solid',
-                    outlineOffset: -3,
-                  },
-                },
-                pressed: {
-                  md: {
-                    backgroundColor: '#6f6f6f',
-                  },
-                },
-                disabled: {
-                  md: {
-                    backgroundColor: '#E4E4E4',
-                  },
-                },
-              },
-              danger: {
-                rest: {
-                  md: {
-                    backgroundColor: '#da1e28',
-                  },
-                },
-                hover: {
-                  md: {
-                    backgroundColor: '#b81922',
-                  },
-                },
-                focus: {
-                  md: {
-                    outlineWidth: 1,
-                    outlineColor: '#ffffff',
-                    outlineStyle: 'solid',
-                    outlineOffset: -3,
-                  },
-                },
-                pressed: {
-                  md: {
-                    backgroundColor: '#750e13',
-                  },
-                },
-                disabled: {
-                  md: {
-                    backgroundColor: '#E4E4E4',
-                  },
-                },
-              },
-            },
-          },
-          outline: {
-            base: {
-              md: {
-                borderWidth: 1,
-                borderStyle: 'solid',
-              },
-            },
-            variant: {
-              primary: {
-                rest: {
-                  md: {
-                    backgroundColor: '#ffffff',
-                    borderColor: '#0F62FE',
-                  },
-                },
-              },
-            },
+        textAlign: {
+          default: 'left',
+          left: true,
+        },
+        icon: {
+          enable: {
+            right: true,
           },
         },
       },
-      leftIcon: {
-        base: {
-          rest: {
-            md: {
-              fontSize: 16,
-              paddingRight: 16,
-              paddingLeft: 16,
-            },
-          },
-        },
-      },
-      text: {
-        base: {
-          rest: {
-            md: {
-              fontSize: '1rem',
-              lineHeight: '1.375rem',
-              fontWeight: 400,
-              fontFamily: 'IBM Plex Sans',
-              fontStyle: 'normal',
-              paddingTop: 13,
-              paddingBottom: 13,
-              paddingLeft: 16,
-              paddingRight: 64,
-            },
-          },
-          disabled: {
-            md: {
-              color: '#8d8d8d',
-            },
-          },
-        },
-        type: {
-          contained: {
-            base: {
-              md: {
-                color: '#FFFFFF',
+      elements: {
+        container: {
+          type: {
+            contained: {
+              variant: {
+                primary: {
+                  rest: {
+                    md: {
+                      backgroundColor: '#0F62FE',
+                    },
+                  },
+                  hover: {
+                    md: {
+                      backgroundColor: '#0353E9 ',
+                    },
+                  },
+                  focus: {
+                    md: {
+                      outlineWidth: 1,
+                      outlineColor: '#ffffff',
+                      outlineStyle: 'solid',
+                      outlineOffset: -3,
+                    },
+                  },
+                  pressed: {
+                    md: {
+                      backgroundColor: '#002D9C',
+                    },
+                  },
+                  disabled: {
+                    md: {
+                      backgroundColor: '#E4E4E4',
+                    },
+                  },
+                },
+                secondary: {
+                  rest: {
+                    md: {
+                      backgroundColor: '#393939',
+                    },
+                  },
+                  hover: {
+                    md: {
+                      backgroundColor: '#474747',
+                    },
+                  },
+                  focus: {
+                    md: {
+                      outlineWidth: 1,
+                      outlineColor: '#ffffff',
+                      outlineStyle: 'solid',
+                      outlineOffset: -3,
+                    },
+                  },
+                  pressed: {
+                    md: {
+                      backgroundColor: '#6f6f6f',
+                    },
+                  },
+                  disabled: {
+                    md: {
+                      backgroundColor: '#E4E4E4',
+                    },
+                  },
+                },
+                danger: {
+                  rest: {
+                    md: {
+                      backgroundColor: '#da1e28',
+                    },
+                  },
+                  hover: {
+                    md: {
+                      backgroundColor: '#b81922',
+                    },
+                  },
+                  focus: {
+                    md: {
+                      outlineWidth: 1,
+                      outlineColor: '#ffffff',
+                      outlineStyle: 'solid',
+                      outlineOffset: -3,
+                    },
+                  },
+                  pressed: {
+                    md: {
+                      backgroundColor: '#750e13',
+                    },
+                  },
+                  disabled: {
+                    md: {
+                      backgroundColor: '#E4E4E4',
+                    },
+                  },
+                },
               },
             },
-          },
-          outline: {
-            base: {
-              md: {
-                paddingTop: 12,
-                paddingBottom: 12,
-                paddingLeft: 15,
-                paddingRight: 63,
+            outline: {
+              base: {
+                md: {
+                  borderWidth: 1,
+                  borderStyle: 'solid',
+                },
               },
-            },
-            variant: {
-              primary: {
-                rest: {
-                  md: {
-                    color: '#0F62FE',
+              variant: {
+                primary: {
+                  rest: {
+                    md: {
+                      backgroundColor: '#ffffff',
+                      borderColor: '#0F62FE',
+                    },
                   },
                 },
               },
             },
           },
         },
-      },
-      rightIcon: {
-        base: {
-          rest: {
-            md: {
-              fontSize: 16,
-              paddingRight: 16,
-              paddingLeft: 16,
+        text: {
+          base: {
+            rest: {
+              md: {
+                fontSize: '1rem',
+                lineHeight: '1.375rem',
+                fontWeight: 400,
+                fontFamily: 'IBM Plex Sans',
+                fontStyle: 'normal',
+                paddingTop: 13,
+                paddingBottom: 13,
+                paddingLeft: 16,
+                paddingRight: 64,
+              },
+            },
+            disabled: {
+              md: {
+                color: '#8d8d8d',
+              },
+            },
+          },
+          type: {
+            contained: {
+              base: {
+                md: {
+                  color: '#FFFFFF',
+                },
+              },
+            },
+            outline: {
+              base: {
+                md: {
+                  paddingTop: 12,
+                  paddingBottom: 12,
+                  paddingLeft: 15,
+                  paddingRight: 63,
+                },
+              },
+              variant: {
+                primary: {
+                  rest: {
+                    md: {
+                      color: '#0F62FE',
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        rightIconDetached: {
+          base: {
+            rest: {
+              md: {
+                fontSize: 16,
+                paddingRight: 16,
+                paddingLeft: 16,
+              },
             },
           },
         },
--- packages/react/src/themes/fluent/theme.ts
+++ packages/react/src/themes/fluent/theme.ts
@@ -7,167 +7,172 @@ export const fluentTheme: KiskadeeSchema = {
   kiskadeeVersion: '0.0.1',
   component: {
     button: {
-      container: {
-        option: {
-          widthMin: 100,
-          borderRadius: {
-            default: 4,
-            rounded: 4,
-          },
-          textAlign: {
-            default: 'center',
-            center: true,
-            left: true,
-          },
-          icon: {
-            rightAttached: true,
-            leftAttached: true,
-            leftDetached: true,
-            rightDetached: true,
-          },
-        },
-        base: {
-          focus: {
-            md: {
-              outlineWidth: 2,
-              outlineStyle: 'solid',
-              outlineColor: '#000',
-              outlineOffset: 1,
+      option: {
+        widthMin: 100,
+        borderRadius: {
+          default: 'rounded',
+          variant: {
+            rounded: {
+              md: 4,
             },
           },
         },
-        type: {
-          contained: {
-            variant: {
-              primary: {
-                rest: {
-                  md: {
-                    backgroundColor: '#005FB8',
-                  },
-                },
-                hover: {
-                  md: {
-                    backgroundColor: '#1A6FBF',
-                  },
-                },
-                pressed: {
-                  md: {
-                    backgroundColor: '#327EC5',
-                  },
-                },
-                disabled: {
-                  md: {
-                    backgroundColor: '#C8C8C8',
-                  },
-                },
-              },
-              secondary: {
-                rest: {
-                  md: {
-                    backgroundColor: '#FFFFFFB2',
-                    borderWidth: 1,
-                    borderStyle: 'solid',
-                    borderColor: '#0000000F',
-                  },
-                },
-                hover: {
-                  md: {
-                    backgroundColor: '#F9F9F980',
-                  },
-                },
-                pressed: {
-                  md: {
-                    backgroundColor: '#F9F9F94D',
-                  },
-                },
-                disabled: {
-                  md: {
-                    backgroundColor: '#F9F9F94D',
-                  },
-                },
-                focus: {
-                  md: {
-                    backgroundColor: '#FFFFFFB2',
-                  },
-                },
-              },
-            },
+        textAlign: {
+          default: 'center',
+          center: true,
+          left: true,
+        },
+        icon: {
+          enable: {
+            right: true,
           },
         },
       },
-      leftIcon: {
-        base: {
-          rest: {
-            md: {
-              paddingLeft: 8,
-              paddingRight: 8,
-              fontSize: 16,
-            },
-          },
-        },
-      },
-      text: {
-        base: {
-          rest: {
-            md: {
-              fontSize: '0.875rem',
-              lineHeight: '20px',
-              fontWeight: 600,
-              fontFamily: 'Segoe UI',
-              fontStyle: 'normal',
-            },
-          },
-        },
-        type: {
-          contained: {
-            base: {
+      elements: {
+        container: {
+          base: {
+            focus: {
               md: {
-                paddingTop: 4,
-                paddingBottom: 6,
-                paddingLeft: 11,
-                paddingRight: 11,
+                outlineWidth: 2,
+                outlineStyle: 'solid',
+                outlineColor: '#000',
+                outlineOffset: 1,
               },
             },
-            variant: {
-              primary: {
-                rest: {
-                  md: {
-                    color: '#FFFFFF',
+          },
+          type: {
+            contained: {
+              variant: {
+                primary: {
+                  rest: {
+                    md: {
+                      backgroundColor: '#005FB8',
+                    },
+                  },
+                  hover: {
+                    md: {
+                      backgroundColor: '#1A6FBF',
+                    },
+                  },
+                  pressed: {
+                    md: {
+                      backgroundColor: '#327EC5',
+                    },
+                  },
+                  disabled: {
+                    md: {
+                      backgroundColor: '#C8C8C8',
+                    },
                   },
                 },
-                pressed: {
-                  md: {
-                    color: '#ffffffb3',
+                secondary: {
+                  rest: {
+                    md: {
+                      backgroundColor: '#FFFFFFB2',
+                      borderWidth: 1,
+                      borderStyle: 'solid',
+                      borderColor: '#0000000F',
+                    },
                   },
-                },
-              },
-              secondary: {
-                rest: {
-                  md: {
-                    color: '#000000E4',
-                    paddingTop: 3,
-                    paddingBottom: 5,
-                    paddingLeft: 10,
-                    paddingRight: 10,
+                  hover: {
+                    md: {
+                      backgroundColor: '#F9F9F980',
+                    },
                   },
-                },
-                disabled: {
-                  md: {
-                    color: '#0000005C',
+                  pressed: {
+                    md: {
+                      backgroundColor: '#F9F9F94D',
+                    },
+                  },
+                  disabled: {
+                    md: {
+                      backgroundColor: '#F9F9F94D',
+                    },
+                  },
+                  focus: {
+                    md: {
+                      backgroundColor: '#FFFFFFB2',
+                    },
                   },
                 },
               },
             },
           },
         },
-      },
-      rightIcon: {
-        base: {
-          rest: {
-            md: {
-              paddingLeft: 8,
-              paddingRight: 8,
-              fontSize: 16,
+        leftIconAttached: {
+          base: {
+            rest: {
+              md: {
+                paddingLeft: 8,
+                paddingRight: 8,
+                fontSize: 16,
+              },
+            },
+          },
+        },
+        text: {
+          base: {
+            rest: {
+              md: {
+                fontSize: '0.875rem',
+                lineHeight: '20px',
+                fontWeight: 600,
+                fontFamily: 'Segoe UI',
+                fontStyle: 'normal',
+              },
+            },
+          },
+          type: {
+            contained: {
+              base: {
+                md: {
+                  paddingTop: 4,
+                  paddingBottom: 6,
+                  paddingLeft: 11,
+                  paddingRight: 11,
+                },
+              },
+              variant: {
+                primary: {
+                  rest: {
+                    md: {
+                      color: '#FFFFFF',
+                    },
+                  },
+                  pressed: {
+                    md: {
+                      color: '#ffffffb3',
+                    },
+                  },
+                },
+                secondary: {
+                  rest: {
+                    md: {
+                      color: '#000000E4',
+                      paddingTop: 3,
+                      paddingBottom: 5,
+                      paddingLeft: 10,
+                      paddingRight: 10,
+                    },
+                  },
+                  disabled: {
+                    md: {
+                      color: '#0000005C',
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        rightIconAttached: {
+          base: {
+            rest: {
+              md: {
+                paddingLeft: 8,
+                paddingRight: 8,
+                fontSize: 16,
+              },
             },
           },
         },
