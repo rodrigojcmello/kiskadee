4ba542fc17509753ca57e1d6f87abd98f98f13ba
Rodrigo Mello <rodrigojcmello@gmail.com>
2022-07-05 22:27:53 -0300

    feat: add routing

--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -1,14 +1,43 @@
 import type { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { Bar } from './site/components/Bar/Bar';
 import { schema } from './themes/schema';
 import { KiskadeeProvider } from './context';
 import { ButtonDocumentation } from './site/documentation/Button/ButtonDocumentation';
+import { Button } from './components/Button';
 
+const ButtonDocument = () => {
+  return (
+    <>
+      <Bar />
+      <ButtonDocumentation />
+    </>
+  );
+};
+
+const ButtonOnly = () => {
+  return (
+    <>
+      <Bar />
+      <div style={{ margin: 100 }}>
+        <Button
+          type={'contained'}
+          variant={'primary'}
+          label={'text text text'}
+        />
+      </div>
+    </>
+  );
+};
+
+// TODO: packages/react become packages/docs
 export const App: FC = () => {
   return (
     <KiskadeeProvider schema={schema} only={'dark'}>
-      <Bar />
-      <ButtonDocumentation />
+      <Routes>
+        <Route path={'/'} element={<ButtonDocument />} />
+        <Route path={'button'} element={<ButtonOnly />} />
+      </Routes>
     </KiskadeeProvider>
   );
 };
