7e09af9aa750644b3dcc31ed3be41b1b6a815d9c
Rodrigo Mello <rodrigojcmello@gmail.com>
2023-01-18 10:53:17 -0300

    chore: update dependencies

--- packages/documentation/vite.config.ts
+++ packages/documentation/vite.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-default-export */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// @ts-ignore
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // @ts-ignore
   plugins: [svgr(), react()],
 });
